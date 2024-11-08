011ef178590f06f26262d7574dfe8bce140fe4fc
cacazi <00001094842643sp@al.educacao.sp.gov.br>
2024-11-08 11:27:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é github?',
+    'O github é um repositorio'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de sao paulo?',
+    'A capital de sao paulo é sp'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bomba em Inglês?',
+    'bomba em ingles é pump'
+)
